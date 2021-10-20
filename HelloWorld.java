46458ccfa33bc7b616d40e54bd1804d12fc4a2a5
Panagiotis Georgios Malekas <92882505+geomalgr@users.noreply.github.com>
2021-10-20 21:06:01 +0300

    update2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
   public static void main (String [] args) {
     System.out.println("Hello world");
-    //this is an addition for github learning purposes
     //this is another comment in replacement of the first one
   }
 }
